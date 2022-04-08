384f3716221187760025e6cbe269c26e9bf9c689
unknown <da4adindu@gmail.com>
2022-04-08 02:33:01 +0100

    project complete?

--- php-projects/countdown-app/index.php
+++ php-projects/countdown-app/index.php
@@ -8,8 +8,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- CSS & Bootstrap -->
-    <!-- <link href="NAME OF CSS FILE .CSS" rel="stylesheet"> -->
+    <style>
+        p {
+            font-size: 30px;
+            margin: 20px 0px 20px 0px;
+        }
+
+        .container {
+            text-align: center;
+            margin: 20px 0px;
+        }
+
+        h1 {
+            background-color: red;
+            padding: 12px;
+        }
+
+        #demo {
+            text-decoration: underline;
+        }
+
+    </style>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
@@ -20,11 +39,12 @@
   <body>
 
     <?php 
-        $date = date('2021-02-18');
-        $time = date('12:25:00');
-        $date_today = $date . '' . $time;
+        $date = date('2022-04-22');
+        $time = date('14:00:00');
+        $date_today = $date . ' ' . $time;
     ?>
-    <script>
+
+    <script type="text/javascript">
       // set the date we are counting down to 
       let count_id = "<?php echo $date_today; ?>";
       let countDownDate = new Date(count_id).getTime();
@@ -34,16 +54,38 @@
         // get todays date and time
         let now = new Date().getTime();
         // find the disatance between now and the count down date
-        let distance = count - now;
+        let distance = countDownDate - now;
         // time calculations for day,hours,minutes and seconds
-        let days = Math.floor(distance/(1000 * 60 * 60 * 24));
+        let days = Math.floor(distance/(1000*60*60*24));
         let hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
         let minutes = Math.floor((distance%(1000*60*60))/(1000*60));
-        let seconds = Math.floor((distance%(1000*60))/(1000));
+        let seconds = Math.floor((distance%(1000*60))/1000);
 
         // output countdown 
-      });
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+        // display when count down is finished
+        if(distance<0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "";
+        }
+      }, 1000);
     </script> 
+
+    <div class="container">
+
+        <h1>
+            Welcome David, hope your having a nice day!
+        </h1>
+
+        <p class="mt-5 mb-5">This is how long you have till your interview with Champion's Travel</p>
+        <span style="font-size: 20px;"> <?php echo $date . " at " . $time; ?> </span>
+        <br>
+        <span id="demo" style="font-size:40px;"></span>
+
+    </div>
+
+    
     
 
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
